c7dfbe19651f05a946e4f2b513b7ec104f7aa111
AmirhoseinAKZ <amirh.ak322@gmail.com>
2024-01-04 15:47:24 +0330

    fix: js code for icons also fixed!

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     if(this.classList.contains('active')){
-      this.querySelector('img').setAttribute('src', "/assets/images/icon-plus.svg");
+      this.querySelector('img').setAttribute('src', "assets/images/icon-plus.svg");
     }
     else{
-      this.querySelector('img').setAttribute('src', "/assets/images/icon-minus.svg");
+      this.querySelector('img').setAttribute('src', "assets/images/icon-minus.svg");
     }
     this.classList.toggle("active");
 
